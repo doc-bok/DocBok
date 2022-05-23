3d0a7f8aabe8d099186af9635fa342bd5d257392
doc-bok <41854155+doc-bok@users.noreply.github.com>
2022-05-23 08:34:05 +0800

    DB-28: Added peacemaker butterfly movement behaviour

--- src/main/java/com/bokmcdok/cat/objects/entities/PeacemakerButterfly.java
+++ src/main/java/com/bokmcdok/cat/objects/entities/PeacemakerButterfly.java
@@ -1,19 +1,25 @@
 package com.bokmcdok.cat.objects.entities;
 
-import com.bokmcdok.cat.objects.models.PeacemakerButterflyModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomFlyingGoal;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
 
@@ -39,21 +45,84 @@ public class PeacemakerButterfly extends Monster {
                                           @SuppressWarnings("unused") Random rng) {
         return level.getRawBrightness(position, 0) > 8;
     }
+
     /**
      * Create attributes for a butterfly.
      * @return Butterflies have only 3 health (1.5 hearts).
      */
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 3d);
+        return Monster.createMonsterAttributes()
+                .add(Attributes.MAX_HEALTH, 6.0D)
+                .add(Attributes.ATTACK_DAMAGE, 3.0D)
+                .add(Attributes.FLYING_SPEED, 0.9D);
     }
 
+    /**
+     * Create a peacemaker butterfly entity
+     * @param type The entity type
+     * @param level The current level
+     */
     public PeacemakerButterfly(EntityType<? extends PeacemakerButterfly> type, Level level) {
         super(type, level);
+
+        //  Add a flying move controller
+        this.moveControl = new FlyingMoveControl(this, 20, true);
+        this.setPathfindingMalus(BlockPathTypes.DANGER_FIRE, -1.0F);
+        this.setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, -1.0F);
     }
 
+    /**
+     * Butterflies ignore all fall damage.
+     * @param fallDistance The distance fell.
+     * @param blockModifier The modifier based on the softness of the block landed on.
+     * @param damageSource The source of the damage.
+     * @return Always false, to indicate no damage was applied.
+     */
+    @Override
+    public boolean causeFallDamage(float fallDistance, float blockModifier, @NotNull DamageSource damageSource) {
+        return false;
+    }
+
+    /**
+     * Butterflies ignore fall damage.
+     * @param yPos The current y-position of the butterfly.
+     * @param onGround TRUE if the butterfly is on the ground.
+     * @param block The block landed on.
+     * @param position The position of the block being landed on.
+     */
+    @Override
+    protected void checkFallDamage(double yPos, boolean onGround, @NotNull BlockState block, @NotNull BlockPos position) {
+        //  No-op
+    }
+
+    /**
+     * Create a pathfinder for flying
+     * @param level The current level
+     * @return A flying navigator
+     */
+    @Override
+    protected @NotNull PathNavigation createNavigation(@NotNull Level level) {
+        FlyingPathNavigation navigation = new FlyingPathNavigation(this, level);
+        navigation.setCanOpenDoors(false);
+        navigation.setCanFloat(true);
+        navigation.setCanPassDoors(true);
+        return navigation;
+    }
+
+    /**
+     * Register the goals that define the entity's behaviour
+     */
     @Override
     protected void registerGoals() {
+
+        //  Movement goals
+        this.goalSelector.addGoal(5, new FloatGoal(this));
+        //this.goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1.0D, true, 4, () -> false));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomFlyingGoal(this, 1.0D));
+
+        //  Look at goals
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
     }
+
 }
