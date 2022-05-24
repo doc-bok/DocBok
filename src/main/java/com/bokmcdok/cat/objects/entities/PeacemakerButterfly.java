49a6e5d860c78d015a8d0ea00c74fe319c58a751
doc-bok <41854155+doc-bok@users.noreply.github.com>
2022-05-24 10:44:33 +0800

    DB-28: Peacemaker Butterflies are now tempted by peacemaker honey

--- src/main/java/com/bokmcdok/cat/objects/entities/PeacemakerButterfly.java
+++ src/main/java/com/bokmcdok/cat/objects/entities/PeacemakerButterfly.java
@@ -1,5 +1,6 @@
 package com.bokmcdok.cat.objects.entities;
 
+import com.bokmcdok.cat.lists.ItemList;
 import com.bokmcdok.cat.objects.goals.FlyThroughVillageGoal;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.NbtOps;
@@ -16,6 +17,7 @@ import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomFlyingGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
@@ -29,6 +31,8 @@ import net.minecraft.world.entity.monster.ZombieVillager;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.biome.Biome;
@@ -226,6 +230,9 @@ public class PeacemakerButterfly extends Monster {
 
         //  Attack goals
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+
+        //  Tempt goals
+        this.goalSelector.addGoal(1, new TemptGoal(this, 1.25D, Ingredient.of(ItemList.PEACEMAKER_HONEY_BOTTLE.get()), false));
 
         //  Targets
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(PeacemakerButterfly.class));
